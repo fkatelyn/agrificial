e3a59a74a95cbb81e7f2d3028f5d3c968c3fedde
Katelyn Fritz <fkatelyn@gmail.com>
2023-11-26 08:35:27 +0800

    Add placeholder for Agrificial XCode project

---
+++ app/Agrificial/Agrificial/AgrificialApp.swift
@@ -0,0 +1,17 @@
+//
+//  AgrificialApp.swift
+//  Agrificial
+//
+//  Created by Katelyn Fritz on 11/26/23.
+//
+
+import SwiftUI
+
+@main
+struct AgrificialApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
